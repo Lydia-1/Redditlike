017179245e5566794cd7cd31dd52fde82200bb93
lydia <lydia-france@live.com>
2015-05-07 16:35:41 +0200

    Add names to user model and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	 # Prevent CSRF attacks by raising an exception.
+	 # For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+
+# on va mettre un nom donc pour ne pas updater un standard qui est plus existant
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.for(:sign_up) << :name
+			devise_parameter_sanitizer.for(:account_update) << :name
+		end
 end
